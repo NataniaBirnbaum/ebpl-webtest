4fca3d4f42150025ad2e42e4e27b1922f6b50050
Natania Birnbaum <64991332+NataniaBirnbaum@users.noreply.github.com>
2022-12-15 19:34:02 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,4 +6,23 @@
 p5js/4.2-nested-loops.html // http://youtu.be/1c1_TMdf8b8
 // https://editor.p5js.org/codingtrain/sketches/nxJTfiJd
 
-function setup() { let canvas = createCanvas(300, 300); canvas.parent('sketch-container'); background(0); } function draw() { strokeWeight(4); stroke(255); for (var x = 0; x <= width; x += 50) { for (var y = 0; y <= height; y += 50) { fill(random(255), 0, random(255)); ellipse(x, y, 25, 25); } } } function drawBackground() { background(random(255), random(255), random(255)); }
+function setup() {
+  let canvas = createCanvas(300, 300);
+  canvas.parent('sketch-container');
+  background(0);
+}
+
+function draw() {
+  strokeWeight(4);
+  stroke(255);
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= height; y += 50) {
+      fill(random(255), 0, random(255));
+      ellipse(x, y, 25, 25);
+    }
+  }
+}
+
+function drawBackground() {
+  background(random(255), random(255), random(255));
+}
